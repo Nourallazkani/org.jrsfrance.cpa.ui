c1b171e2307f54f6cf10c44f92b4507ec6d47114
Alaric <alaric_hermant@hotmail.com>
2016-07-15 20:04:26 -0500

    bug fix on router

--- src/app.js
+++ src/app.js
@@ -4,10 +4,20 @@ import {HttpClient} from 'aurelia-fetch-client';
 import {Router} from 'aurelia-router';
 import {Global} from 'global'
 
-@inject(Router, HttpClient, Global)
+@inject(HttpClient, Global)
 export class App {
 
-  constructor(router, httpClient, global) {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'index'], name: 'index', moduleId: './views/index', nav: true, title: 'Index' },
+      { route: ['home'], name: 'home', moduleId: './views/home', nav: true, title: 'Accueil' },
+      { route: ['refugees'], name: 'refugees/index', moduleId: './views/refugees/index', nav: true, title: 'Réfugiés' }
+    ]);
+    this.router = router;
+  }
+
+  constructor(httpClient, global) {
     httpClient.configure(config => {
       config
         .withBaseUrl('http://127.0.0.1:8080/api/')
@@ -27,24 +37,5 @@ export class App {
     })
 
     httpClient.fetch("referenceData").then(x => x.json()).then(x => global.referenceData = x)
-
-    this.router = router
-    this.router.configure(config => {
-      //config.options.pushState = true;
-      config.title = "CPA";
-      config.map([
-        { route: ['', 'Index'], name: 'index', moduleId: 'views/index', nav: true, title: 'Index' },
-        { route: ['Home'], name: 'home', moduleId: 'views/home', nav: true, title: 'Accueil' },
-        { route: ['Refugees'], name: 'refugees/index', moduleId: 'views/refugees/index', nav: true, title: 'Réfugiés' }
-        /*
-        { route: ['events'], moduleId: 'views/refugees/events/events', nav: true, title: 'Réfugiés > Evenements' },
-        { route: ['teachings'], moduleId: 'views/refugees/teachings/teachings', nav: true, title: 'Réfugiés > Enseignements supérieurs' },
-        { route: ['cursus'], moduleId: 'views/refugees/cursus/cursus', nav: true, title: 'Réfugiés > Apprentissage du français' },
-        { route: ['workshops'], moduleId: 'views/refugees/workshops/workshops', nav: true, title: 'Réfugiés > Ateliers socio linguistiques' },
-        { route: ['libraries'], moduleId: 'views/refugees/libraries/libraries', nav: true, title: 'Réfugiés > Auto apprentissage' },
-        { route: ['volunteers'], moduleId: 'views/refugees/volunteers/volunteers', nav: true, title: 'Réfugiés > Bénévoles' }*/
-      ])
-
-    })
   }
 }

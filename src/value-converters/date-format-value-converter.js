82ac3747995b4e24498c17ef6c8a8c9555d15821
Alaric <alaric_hermant@hotmail.com>
2016-09-27 17:52:12 +0200

    bug fix i18n

--- src/value-converters/date-format-value-converter.js
+++ src/value-converters/date-format-value-converter.js
@@ -5,15 +5,17 @@ import moment from 'moment';
 import {FrLocale, ArLocale, PrsLocale} from 'moment-locales'
 
 // sera utilisation sous le nom myDateFormat
-@inject(UserDetails, FrLocale)
+@inject(UserDetails, FrLocale, ArLocale, PrsLocale)
 export class MyDateFormatValueConverter {
 
   formaters = { event: "D/M/YYYY h:mm:ss a" }
   defaultFormat = "D/M/YYYY"
 
-  constructor(userDetails, frLocale) {
+  constructor(userDetails, frLocale, arLocale, prsLocale) {
     this.userDetails = userDetails;
     moment.locale("fr", frLocale);
+    moment.locale("ar", arLocale);
+    moment.locale("prs", prsLocale);
   }
 
   toView(value, format) {

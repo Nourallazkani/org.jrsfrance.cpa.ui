1064e7c9558f944b3067d01c5891208c3430ae9f
Alaric <alaric_hermant@hotmail.com>
2017-01-08 16:28:18 +0100

    polish deploy task

--- aurelia_project/tasks/deploy.js
+++ aurelia_project/tasks/deploy.js
@@ -6,29 +6,31 @@ import {CLIOptions} from 'aurelia-cli';
 export default function deploy(done) {
     var awsCredentials = JSON.parse(fs.readFileSync('./awscredentials.json'))
 
-    var toUpload = ["./index.html", "./scripts/**/**", "./assets/**/**"];
-
     var uploader = s3(awsCredentials);
     let uploaderOptions = { maxRetries: 5, region: "eu-west-1" };
-    if (CLIOptions.instance.args.includes('--target')) {
-        let target = CLIOptions.instance.args[CLIOptions.instance.args.indexOf("--target") + 1];
-        console.log(`deploy ${toUpload.join(', ')} to ${target}`)
-        gulp.src(toUpload, { base: './' })
-            .pipe(uploader({ Bucket: target, ACL: 'public-read' }, uploaderOptions))
-            .on('end', () => done());
+    
+    let buckets;
+    if (CLIOptions.instance.args.includes('--buckets')) {
+        let target = CLIOptions.instance.args[CLIOptions.instance.args.indexOf("--buckets") + 1];
+        buckets = target.split(",");
     }
-    else {
-        console.log(`deploy ${toUpload.join(', ')} to www2.comprendrepourapprendre.org`)
+    else{
+        buckets = ["www2.comprendrepourapprendre.org", "www2.cpafrance.fr"];
+    }
+
+    var toUpload = ["./index.html", "./scripts/**/**.js", "./assets/**/**"];
+
+    let uploadsDone = 0;
+    for (let bucket of buckets) {
+        console.log(`deploy ${toUpload.join(', ')} to ${bucket}`)
         gulp.src(toUpload, { base: './' })
-            .pipe(uploader({ Bucket: 'www2.comprendrepourapprendre.org', ACL: 'public-read' }, uploaderOptions))
+            .pipe(uploader({ Bucket: bucket, ACL: 'public-read' }, uploaderOptions))
             .on('end', () => {
-                console.log(`deploy ${toUpload.join(', ')} to www2.cpafrance.fr`)
-                gulp.src(toUpload, { base: './' })
-                    .pipe(uploader({ Bucket: 'www2.cpafrance.fr', ACL: 'public-read' }, uploaderOptions))
-                    .on('end', () => {
-                        console.log("done");
-                        done()
-                    })
+                console.log(`deploy ${toUpload.join(', ')} to ${bucket} : OK`);
+                uploadsDone = uploadsDone + 1;
+                if (uploadsDone == buckets.length) {
+                    done();
+                }
             });
     }
 }

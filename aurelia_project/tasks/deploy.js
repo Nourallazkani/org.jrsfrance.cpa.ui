c0dd813851aeadce23f74fd2f9c322b88ade87ca
Alaric <alaric_hermant@hotmail.com>
2017-01-09 00:07:11 +0100

    deploy polish

--- aurelia_project/tasks/deploy.js
+++ aurelia_project/tasks/deploy.js
@@ -4,37 +4,37 @@ import gulp from 'gulp';
 import build from './build';
 import {CLIOptions} from 'aurelia-cli';
 
-export default gulp.series(build, deploy);
+var awsCredentials = JSON.parse(fs.readFileSync('./awscredentials.json'))
 
-function deploy(done) {
-    var awsCredentials = JSON.parse(fs.readFileSync('./awscredentials.json'))
+var uploader = s3(awsCredentials);
+let uploaderOptions = { maxRetries: 5, region: "eu-west-1" };
 
-    var uploader = s3(awsCredentials);
-    let uploaderOptions = { maxRetries: 5, region: "eu-west-1" };
-
-    let buckets;
-    if (CLIOptions.instance.args.includes('--buckets')) {
-        let target = CLIOptions.instance.args[CLIOptions.instance.args.indexOf("--buckets") + 1];
-        buckets = target.split(",");
-    }
-    else {
-        buckets = ["www2.comprendrepourapprendre.org", "www2.cpafrance.fr"];
-    }
-
-    var toUpload = ["./index.html", "./scripts/**/**.js", "./assets/**/**"];
-
-
-    let uploadsDone = 0;
-    for (let bucket of buckets) {
-        console.log(`deploy ${toUpload.join(', ')} to ${bucket}`)
-        gulp.src(toUpload, { base: './' })
-            .pipe(uploader({ Bucket: bucket, ACL: 'public-read' }, uploaderOptions))
-            .on('end', () => {
-                console.log(`deploy ${toUpload.join(', ')} to ${bucket} : OK`);
-                uploadsDone = uploadsDone + 1;
-                if (uploadsDone == buckets.length) {
-                    done();
-                }
-            });
-    }
+let buckets;
+if (CLIOptions.instance.args.includes('--buckets')) {
+    let target = CLIOptions.instance.args[CLIOptions.instance.args.indexOf("--buckets") + 1];
+    buckets = target.split(",");
 }
+else {
+    buckets = ["www2.comprendrepourapprendre.org", "www2.cpafrance.fr"];
+}
+
+var toUpload = ["./index.html", "./scripts/**/**.js", "./assets/**/**"];
+
+let functions = buckets.map(bucket => function (_done) {
+    console.log(`Starting upload of ${toUpload.join(', ')} to ${bucket}...`);
+    gulp.src(toUpload, { base: './' })
+        .pipe(uploader({ Bucket: bucket, ACL: 'public-read' }, uploaderOptions))
+        .on('end', () => {
+            console.log(`Finished upload of ${toUpload.join(', ')} to ${bucket}...`);
+            _done();
+        });
+});
+
+if(!CLIOptions.instance.args.includes('--env')){
+    CLIOptions.instance.args.push('--env', 'prod');
+}
+else{
+    CLIOptions.instance.args[CLIOptions.instance.args.indexOf("--env") + 1] = "prod";
+}
+
+export default gulp.series(build, functions);
